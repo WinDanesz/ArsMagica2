f50e4f606a13b1b8e9fc3c3fb916731514715263
TheIcyOne <icy.headfishindustries@gmail.com>
2017-07-02 20:46:16 +0100

    Changes for build
    Should be possible to build the repo now.

--- src/main/java/am2/compat/jei/EssenceRefinerRecipeWrapper.java
+++ src/main/java/am2/compat/jei/EssenceRefinerRecipeWrapper.java
@@ -1,16 +1,20 @@
 package am2.compat.jei;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 import am2.api.recipes.RecipeArsMagica;
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.BlankRecipeWrapper;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
-public class EssenceRefinerRecipeWrapper implements IRecipeWrapper {
+public class EssenceRefinerRecipeWrapper extends BlankRecipeWrapper {
 	
 	RecipeArsMagica recipe;
 	
@@ -19,43 +23,13 @@ public class EssenceRefinerRecipeWrapper implements IRecipeWrapper {
 	}
 	
 	@Override
-	public List<?> getInputs() {
-		return Lists.newArrayList(recipe.getRecipeItems());
-	}
-
-	@Override
-	public List<?> getOutputs() {
-		return Lists.newArrayList(recipe.getOutput());
-	}
-
-	@Override
-	public List<FluidStack> getFluidInputs() {
-		return ImmutableList.of();
-	}
-
-	@Override
-	public List<FluidStack> getFluidOutputs() {
-		return ImmutableList.of();
-	}
-
-	@Override
-	public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
-		
-	}
-
-	@Override
-	public void drawAnimations(Minecraft minecraft, int recipeWidth, int recipeHeight) {
-		
-	}
-
-	@Override
-	public List<String> getTooltipStrings(int mouseX, int mouseY) {
-		return null;
-	}
-
-	@Override
-	public boolean handleClick(Minecraft minecraft, int mouseX, int mouseY, int mouseButton) {
-		return false;
+	public void getIngredients(IIngredients ingredients) {
+		List<ItemStack> inputs = new ArrayList<ItemStack>();
+		for (ItemStack item : recipe.getRecipeItems()){
+			inputs.add(item);
+		}
+		ingredients.setInputs(ItemStack.class, inputs);
+		ingredients.setOutput(ItemStack.class, recipe.getOutput());
 	}
 
 }
