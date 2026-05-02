e4a0ec53a757a5124c0ae54d4c06f2cfef662a1f
WinDanesz <31292708+windanesz@users.noreply.github.com>
2026-05-03 01:12:16 +0200

    feat: Add ItemBlockManaBattery and tooltip for mana battery

---
+++ src/main/java/am2/common/items/ItemBlockManaBattery.java
@@ -0,0 +1,27 @@
+package am2.common.items;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class ItemBlockManaBattery extends ItemBlock {
+
+    public ItemBlockManaBattery(Block block) {
+        super(block);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        tooltip.add(I18n.format("am2.tooltip.mana_battery"));
+    }
+}
