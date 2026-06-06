1ae278a7749f8041216e0368b3bce0dca158bbd5
Rosan <rosan1233456@gmail.com>
2026-06-06 22:46:07 +0300

    fix lectern render

--- src/main/java/am2/client/blocks/render/TileLecternRenderer.java
+++ src/main/java/am2/client/blocks/render/TileLecternRenderer.java
@@ -2,9 +2,11 @@ package am2.client.blocks.render;
 
 import am2.client.models.ModelArchmagePodium;
 import am2.common.blocks.BlockLectern;
+import am2.common.blocks.tileentity.TileEntityArcaneDeconstructor;
 import am2.common.blocks.tileentity.TileEntityLectern;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBook;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.GlStateManager.DestFactor;
 import net.minecraft.client.renderer.GlStateManager.SourceFactor;
@@ -14,34 +16,28 @@ import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderEntityItem;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
+import org.lwjgl.opengl.GL11;
+
+import java.util.Random;
 
 public class TileLecternRenderer extends TileEntitySpecialRenderer<TileEntityLectern> {
 
     public static final ResourceLocation TEXTURE = new ResourceLocation("arsmagica2", "textures/blocks/custom/lectern.png");
 
-    RenderEntityItem renderItem = new RenderEntityItem(Minecraft.getMinecraft().getRenderManager(), Minecraft.getMinecraft().getRenderItem());
     private final ModelBook enchantmentBook = new ModelBook();
-    private final ModelArchmagePodium podium = new ModelArchmagePodium();
-    private static int curDye = 1;
-    private static int curTick = 0;
-    private static int dyeTickCounter = 0;
 
-    public TileLecternRenderer() {
 
-    }
-
-    public void renderTileEntityArchmagePodiumAt(TileEntityLectern podium, double x, double y, double z, float f1) throws Exception {
+    public void renderTileEntityArchmagePodiumAt(TileEntityLectern podium, double x, double y, double z, float partialTicks) throws Exception {
         Minecraft.getMinecraft().profiler.startSection("Lectern-Render");
         // Old podium model rendering removed - now using JSON block model
         // Only render the book on top via TESR
-        RenderHelper.disableStandardItemLighting();
-        GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
 
         Minecraft.getMinecraft().profiler.startSection("book-model");
         if (podium.hasStack()) {
@@ -49,15 +45,20 @@ public class TileLecternRenderer extends TileEntitySpecialRenderer<TileEntityLec
                 GlStateManager.color(0.7f, 0.2f, 0.2f, 1.0f);
             else
                 GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
-            RenderBook(podium, x, y, z, f1, 0);
-        } else if (podium.getNeedsBook()) {
+            renderBook(podium, x, y, z, partialTicks, 0);
+        }
+        else if (podium.getNeedsBook()) {
             GlStateManager.color(0.7f, 0.2f, 0.2f, 0.2f);
             GlStateManager.enableBlend();
             GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
-            RenderBook(podium, x, y, z, f1, 0);
+            renderBook(podium, x, y, z, partialTicks, 0);
             GlStateManager.disableBlend();
         }
-        renderHelperIcon(podium, x, y, z, f1);
+        else {
+            podium.resetParticleAge();
+            return;
+        }
+        renderHelperIcon(podium, x, y, z, partialTicks);
         GlStateManager.disableBlend();
         GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
         RenderHelper.enableStandardItemLighting();
@@ -65,111 +66,93 @@ public class TileLecternRenderer extends TileEntitySpecialRenderer<TileEntityLec
         Minecraft.getMinecraft().profiler.endSection();
     }
 
-    private void renderHelperIcon(TileEntityLectern podium, double x, double y, double z, float f) {
-        if (podium.getTooltipStack() == null) {
+    private void renderHelperIcon(TileEntityLectern podium, double x, double y, double z, float partialTicks) {
+        ItemStack sus = podium.getTooltipStack();
+        if (sus == null || sus.isEmpty()) {
             podium.resetParticleAge();
             return;
         }
 
-        float deg = (curTick / 0.25f);
+        float deg = (podium.tickCount % 3600);
 
-
-        ItemStack stack = podium.getTooltipStack().copy();
+        ItemStack stack = sus.copy();
         stack.setCount(1);
         Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        GlStateManager.translate(x + 0.5f, y + 1.4f, z + 0.5f);
-        GlStateManager.rotate(deg, 0, f, 0);
-        curTick++;
-        if (curTick >= 3600) { // Reset every 3600 render calls (=40 full rotations, since one rotation = 90 render calls), ensures smooth wrap at 0°/14400°
-            curTick = 0;
-        }
-        dyeTickCounter++;
-        if (dyeTickCounter >= 100) {
-            dyeTickCounter = 0;
-            curDye++;
-            if (curDye >= 16)
-                curDye = 1;
 
-        }
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x + 0.5F, y + 1.65F, z + 0.5F);
+        GlStateManager.rotate(deg, 0, partialTicks, 0);
+
+        int dye = ((podium.tickCount) / 100) % 16;
         if (stack.getItem() == Items.DYE) {
-            stack = new ItemStack(Items.DYE, 1, curDye);
+            stack = new ItemStack(Items.DYE, 1, dye);
         }
         Minecraft.getMinecraft().getRenderItem().renderItem(stack, ItemCameraTransforms.TransformType.GROUND);
-        //try {
-        //renderItem.doRender(AMGuiHelper.instance.dummyItem, x + 0.5f, y + 1.4f, z + 0.5f, AMGuiHelper.instance.dummyItem.rotationYaw, f);
-        //} catch (NullPointerException e) {
-        //	renderItem = new RenderEntityItem(Minecraft.getMinecraft().getRenderManager(), Minecraft.getMinecraft().getRenderItem());
-        //}
+        GlStateManager.popMatrix();
+
         GlStateManager.pushMatrix();
-        GlStateManager.translate(x + 0.5f, y + 1f, z + 0.5f);
-        float scale = 0.2f;
+        GlStateManager.translate(x + 0.5F, y + 1.25F, z + 0.5F);
+        final float scale = 0.2F;
         GlStateManager.scale(scale, scale, scale);
-        renderRadiant(Tessellator.getInstance(), f, podium);
+        renderRadiant(partialTicks, podium);
         GlStateManager.popMatrix();
     }
 
-    private void renderRadiant(Tessellator tessellator, float partialFrame, TileEntityLectern podium) {
-//		RenderHelper.disableStandardItemLighting();
-//		float var4 = (podium.particleAge + partialFrame) / podium.particleMaxAge;
-//		float var5 = 0.0F;
-//
-//		if (var4 > 0.8F){
-//			var5 = (var4 - 0.8F) / 0.2F;
-//		}
-//
-//		Random var6 = new Random(432L);
-//		GL11.glDisable(GL11.GL_TEXTURE_2D);
-//		GL11.glShadeModel(GL11.GL_SMOOTH);
-//		GL11.glEnable(GL11.GL_BLEND);
-//		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
-//		GL11.glDisable(GL11.GL_ALPHA_TEST);
-//		GL11.glEnable(GL11.GL_CULL_FACE);
-//		GL11.glDepthMask(false);
-//		GL11.glPushMatrix();
-//
-//		for (int var7 = 0; var7 < 20.0F; ++var7){
-//			float rH = 90f;
-//			float oH = 0f;
-//			GL11.glPushMatrix();
-//			GL11.glRotatef(podium.field_145926_a % 360, 0.0F, 1.0F, 0.0F);
-//			GL11.glRotatef(var6.nextFloat() * 180, 1.0F, 0.0F, 0.0F);
-//			GL11.glRotatef(var6.nextFloat() * -180, 1.0F, 0.0F, 0.0F);
-//			tessellator.startDrawing(6);
-//			float var8 = var6.nextFloat() * 2.0F + 2.0F + var5 * 0.5F;
-//			float var9 = var6.nextFloat() * 2.0F + 1.0F + var5 * 2.0F;
-//			if (!podium.getOverpowered())
-//				tessellator.setColorRGBA_F(0.2f, 0.2f, 1.0f, 0.2f);
-//			else
-//				tessellator.setColorRGBA_F(1.0f, 0.2f, 0.2f, 0.2f);
-//			tessellator.addVertex(0.0D, 0.0D, 0.0D);
-//			if (!podium.getOverpowered())
-//				tessellator.setColorRGBA_F(0.2f, 0.2f, 1.0f, 0.0f);
-//			else
-//				tessellator.setColorRGBA_F(1.0f, 0.2f, 0.2f, 0.0f);
-//			tessellator.addVertex(-0.866D * var9, var8, -0.5F * var9);
-//			tessellator.addVertex(0.866D * var9, var8, -0.5F * var9);
-//			tessellator.addVertex(0.0D, var8, 1.0F * var9);
-//			tessellator.addVertex(-0.866D * var9, var8, -0.5F * var9);
-//			tessellator.draw();
-//			GL11.glPopMatrix();
-//		}
-//
-//		GL11.glPopMatrix();
-//		GL11.glDepthMask(true);
-//		GL11.glDisable(GL11.GL_CULL_FACE);
-//		GL11.glDisable(GL11.GL_BLEND);
-//		GL11.glShadeModel(GL11.GL_FLAT);
-//		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-//		GL11.glEnable(GL11.GL_TEXTURE_2D);
-//		GL11.glEnable(GL11.GL_ALPHA_TEST);
-//		RenderHelper.enableStandardItemLighting();
+    private void renderRadiant(float partialTicks, TileEntityLectern podium) {
+		float time = (podium.particleAge + partialTicks) / podium.particleMaxAge;
+		float rescale = 0.0F;
+
+		if (time > 0.8F){
+			rescale = (time - 0.8F) / 0.2F;
+		}
+
+		Random rand = new Random(432L);
+        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+		GL11.glPushAttrib(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_ENABLE_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glShadeModel(GL11.GL_SMOOTH);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+		GL11.glDisable(GL11.GL_ALPHA_TEST);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDepthMask(false);
+
+        Tessellator t= Tessellator.getInstance();
+
+		for (int i = 0; i < 20; ++i){
+			GL11.glPushMatrix();
+			GL11.glRotatef(podium.tickCount % 360, 0.0F, 1.0F, 0.0F);
+			GL11.glRotatef(rand.nextFloat() * 180, 1.0F, 0.0F, 0.0F);
+			GL11.glRotatef(rand.nextFloat() * -180, 1.0F, 0.0F, 0.0F);
+            BufferBuilder buf = t.getBuffer();
+            buf.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
+			float w = rand.nextFloat() * 2.0F + 1.0F + rescale * 2.0F;
+			float h = rand.nextFloat() * 2.0F + 2.0F + rescale * 0.5F;
+            float r = 0.2F, g = 0.2F, b = 1.0F, a = 1.0F;
+			if (podium.getOverpowered()) {
+                r = 1.0F;
+                b = 0.2F;
+            }
+            buf.pos(0.0D, 0.0D, 0.0D).color(r, g, b, 0.2F).endVertex();
+			buf.pos(-0.866D * w, h, -0.5F * w).color(r, g, b, 0.0F).endVertex();
+			buf.pos(0.866D * w, h, -0.5F * w).color(r, g, b, 0.0F).endVertex();
+			buf.pos(0.0D, h, w).color(r, g, b, 0.0F).endVertex();
+			buf.pos(-0.866D * w, h, -0.5F * w).color(r, g, b, 0.0F).endVertex();
+			t.draw();
+			GL11.glPopMatrix();
+		}
+
+        GL11.glDepthMask(true);
+        GL11.glPopAttrib();
+		RenderHelper.enableStandardItemLighting();
     }
 
-    private void RenderBook(TileEntityLectern podium, double x, double y, double z, float partialTicks, int meta) {
+    private void renderBook(TileEntityLectern podium, double x, double y, double z, float partialTicks, int meta) {
         GlStateManager.pushMatrix();
         GlStateManager.translate(x + 0.5F, y + 1.15F, z + 0.5F);
         float var9 = (float) podium.tickCount + partialTicks;
-        GlStateManager.translate(0.0F, 0.1F + MathHelper.sin(var9 * 0.01F) * 0.01F, 0.0F);
+        GlStateManager.translate(0.0F, (MathHelper.sin(var9 * 0.01F) + 1.0F) * 0.01F, 0.0F);
         float f2;
 
         for (f2 = podium.tRot - podium.bookRotation; f2 >= (float) Math.PI; f2 -= ((float) Math.PI * 2F)) ;
@@ -178,16 +161,12 @@ public class TileLecternRenderer extends TileEntitySpecialRenderer<TileEntityLec
             f2 += ((float) Math.PI * 2F);
         }
 
-//		float var11 = podium.bookRotationPrev + f2 * partialTicks;
         EnumFacing facing = EnumFacing.NORTH;
         if (podium.hasWorld())
             facing = podium.getWorld().getBlockState(podium.getPos()).getValue(BlockLectern.FACING);
         GlStateManager.rotate(270 - facing.getHorizontalAngle(), 0, 1, 0);
-//		if (facing == EnumFacing.EAST || facing == EnumFacing.SOUTH)
-//			GlStateManager.translate(0, 0, -1);
-//		if (facing == EnumFacing.WEST || facing == EnumFacing.SOUTH)
-//			GlStateManager.translate(-1, 0, 0);
-        GlStateManager.rotate(70.0F, 0.0F, 0.0F, 1.0F);
+
+        GlStateManager.rotate(67.0F, 0.0F, 0.0F, 1.0F);
         bindTexture(new ResourceLocation("textures/entity/enchanting_table_book.png"));
         float var12 = podium.pageFlipPrev + (podium.pageFlip - podium.pageFlipPrev) * partialTicks + 0.25F;
         float var13 = podium.pageFlipPrev + (podium.pageFlip - podium.pageFlipPrev) * partialTicks + 0.75F;
@@ -197,7 +176,6 @@ public class TileLecternRenderer extends TileEntitySpecialRenderer<TileEntityLec
         var12 = MathHelper.clamp(var12, 0, 1);
         var13 = MathHelper.clamp(var13, 0, 1);
 
-//		float var14 = podium.bookSpreadPrev + (podium.bookSpread - podium.bookSpreadPrev) * partialTicks;
         this.enchantmentBook.setRotationAngles(var9, var12, var13, 1f, 0.0F, 0.0625F, (Entity) null);
         this.enchantmentBook.coverRight.render(0.0625F);
         this.enchantmentBook.coverLeft.render(0.0625F);
@@ -213,63 +191,15 @@ public class TileLecternRenderer extends TileEntitySpecialRenderer<TileEntityLec
     public void render(TileEntityLectern te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
         GlStateManager.pushMatrix();
         try {
+            RenderHelper.disableStandardItemLighting();
+            GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
             renderTileEntityArchmagePodiumAt(te, x, y, z, partialTicks);
-        } catch (Exception e) {
+            RenderHelper.enableStandardItemLighting();
+        }
+        catch (Exception e) {
             am2.ArsMagica.LOGGER.error("LecternRenderer.render exception caught: ", e);
             return;
         }
         GlStateManager.popMatrix();
     }
-
-	/*
-	 * GL11.glPushMatrix();
-        GL11.glTranslatef((float)x + 0.5F, (float)y + 0.75F, (float)z + 0.5F);
-        float f1 = (float)podium.field_145926_a + partialTicks;
-        GL11.glTranslatef(0.0F, 0.1F + MathHelper.sin(f1 * 0.1F) * 0.01F, 0.0F);
-        float f2;
-
-        for (f2 = podium.field_145928_o - podium.field_145925_p; f2 >= (float)Math.PI; f2 -= ((float)Math.PI * 2F))
-        {
-            ;
-        }
-
-        while (f2 < -(float)Math.PI)
-        {
-            f2 += ((float)Math.PI * 2F);
-        }
-
-        float f3 = podium.field_145925_p + f2 * partialTicks;
-        GL11.glRotatef(-f3 * 180.0F / (float)Math.PI, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(80.0F, 0.0F, 0.0F, 1.0F);
-        this.bindTexture(book);
-        float f4 = podium.field_145931_j + (podium.field_145933_i - podium.field_145931_j) * partialTicks + 0.25F;
-        float f5 = podium.field_145931_j + (podium.field_145933_i - podium.field_145931_j) * partialTicks + 0.75F;
-        f4 = (f4 - (float)MathHelper.truncateDoubleToInt((double)f4)) * 1.6F - 0.3F;
-        f5 = (f5 - (float)MathHelper.truncateDoubleToInt((double)f5)) * 1.6F - 0.3F;
-
-        if (f4 < 0.0F)
-        {
-            f4 = 0.0F;
-        }
-
-        if (f5 < 0.0F)
-        {
-            f5 = 0.0F;
-        }
-
-        if (f4 > 1.0F)
-        {
-            f4 = 1.0F;
-        }
-
-        if (f5 > 1.0F)
-        {
-            f5 = 1.0F;
-        }
-
-        float f6 = podium.field_145927_n + (podium.field_145930_m - podium.field_145927_n) * partialTicks;
-        GL11.glEnable(GL11.GL_CULL_FACE);
-        this.enchantmentBook.render((Entity)null, f1, f4, f5, f6, 0.0F, 0.0625F);
-        GL11.glPopMatrix();
-	 */
 }
